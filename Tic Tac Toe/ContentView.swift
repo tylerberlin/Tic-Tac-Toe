d782ba6bb0d4f9b88c00e1789ed25bb2414fa916
tyler0611 <berlity0611@gmail.com>
2024-10-02 09:35:15 -0500

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Tyler Berlin on 10/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
